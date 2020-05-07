3d5ef6743dc44146353ebee4fd4bc28e59ab7f05
unknown <fjmiranda4iuandes.cl>
2020-05-07 09:56:15 -0400

    classes

--- actividad/actividad/Program.cs
+++ actividad/actividad/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace actividad
 {
     class Program
     {
+        private static void SaveDaata(Persona person)
+        {
+            Console.WriteLine("Nombre del archivo: ");
+            string fileName = Console.ReadLine();
+            // Creamos el Stream donde guardaremos nuestro juego
+            FileStream fs = new FileStream(fileName, FileMode.CreateNew);
+
+            Formatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, person);
+            fs.Close();
+
+        }
         static void Main(string[] args)
         {
+
+            
         }
     }
 }
---
+++ actividad/actividad/persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace actividad
+{
+    [Serializable]
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+
+    }
+}
