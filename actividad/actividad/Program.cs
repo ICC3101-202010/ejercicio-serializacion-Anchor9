f635335e6b3d789ff229440e907f6f8160df3bd4
unknown <fjmiranda4iuandes.cl>
2020-05-07 10:07:10 -0400

    binary not working

--- actividad/actividad/Program.cs
+++ actividad/actividad/Program.cs
@@ -11,21 +11,36 @@ namespace actividad
 {
     class Program
     {
-        private static void SaveDaata(Persona person)
+        private static void SaveData(Persona person)
         {
             Console.WriteLine("Nombre del archivo: ");
             string fileName = Console.ReadLine();
             // Creamos el Stream donde guardaremos nuestro juego
             FileStream fs = new FileStream(fileName, FileMode.CreateNew);
 
-            Formatter formatter = new BinaryFormatter();
+            Formatter formatter = new BinaryFormatter()
+            //no entiendoi por que me tira error el binary formatter
             formatter.Serialize(fs, person);
             fs.Close();
 
         }
+        private static Persona Loaddata()
+        {
+            Console.WriteLine("Nombre del archivo: ");
+            string fileName = Console.ReadLine();
+             // Creamos el Stream donde se encuentra nuestro juego
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            IFormatter formatter = new BinaryFormatter();
+            Persona person = formatter.Deserialize(fs) as Persona;
+            fs.Close();
+            return person;
+        }
         static void Main(string[] args)
         {
+            Persona p1 = new Persona();
 
+            SaveData(p1);
+            Loaddata();
             
         }
     }
